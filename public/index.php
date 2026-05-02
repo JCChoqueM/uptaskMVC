05a08ea9c3fcb59cd57ee10fbdbc115afd4bcfd7
Cesar <jcchoque23@gmail.com>
2026-05-02 03:12:25 -0400

    algun

--- public/index.php
+++ public/index.php
@@ -3,18 +3,11 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <script type="module" src="http://localhost:5173/@vite/client"></script>
-</head>
-<body>
-    <h1>Probando live relo2ad</h1>
-</body>
-</html>
-
-<?php
 $router = new Router();
+
+
+ 
+
+// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
