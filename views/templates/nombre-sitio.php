bff2422de5bbb9a1080683b29befd972d9a89294
Cesar <jcchoque23@gmail.com>
2026-07-08 21:11:23 -0400

    sopilote

--- views/templates/nombre-sitio.php
+++ views/templates/nombre-sitio.php
@@ -1,2 +1,2 @@
- <h1 class="uptask">SantiTask</h1>
-    <p class="tagline">Crea y administra tus tareas </p>
+<h1 class="uptask">SantiTask</h1>
+<p class="tagline">Crea y administra tus tareas Sopilote </p>
