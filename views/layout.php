bea1fcdf4ad4fccc5b6ef21781f4cb4329c3cd2d
Cesar <jcchoque23@gmail.com>
2026-07-08 21:03:54 -0400

    actualizado para dar tanto en prod y dev

--- views/layout.php
+++ views/layout.php
@@ -1,16 +1,18 @@
 <?php
 $isDev = $_ENV['ENVIRONMENT'] === 'development';
-
+$manifest = null;
 if (!$isDev) {
-    $manifest = json_decode(
-        file_get_contents(__DIR__ . '/../public/build/.vite/manifest.json'),
-        true
-    );
+    $manifestPath = __DIR__ . '/../public/build/.vite/manifest.json';
+    if (file_exists($manifestPath)) {
+        $manifest = json_decode(file_get_contents($manifestPath), true);
+    } else {
+        // Si no existe manifest, fallback a dev
+        $isDev = true;
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,25 +20,16 @@ if (!$isDev) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Open+Sans&display=swap" rel="stylesheet">
-
     <?php if ($isDev): ?>
         <script type="module" src="http://localhost:5173/@vite/client"></script>
-    <?php else: ?>
-        <link rel="stylesheet" href="/build/<?= $manifest['src/scss/app.scss']['css'][0] ?>">
-    <?php endif ?>
-</head>
-
-<body>
-
-    <?= $contenido ?>
-
-    <?php if ($isDev): ?>
         <script type="module" src="http://localhost:5173/src/js/app.js"></script>
     <?php else: ?>
-        <script src="/build/<?= $manifest['src/js/app.js']['file'] ?>"></script>
+        <link rel="stylesheet" href="/build/<?= $manifest['src/scss/app.scss']['file'] ?? '' ?>">
+        <script src="/build/<?= $manifest['src/js/app.js']['file'] ?? '' ?>"></script>
     <?php endif ?>
-
+</head>
+<body>
+    <?= $contenido ?>
     <?= $script ?? '' ?>
-
 </body>
 </html>
