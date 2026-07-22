b3373f7c481c0912abe0789112b0dc2785e5c3d5
Cesar <jcchoque23@gmail.com>
2026-07-22 01:27:34 -0400

    filtros mas scss

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -1,6 +1,6 @@
 <?php include_once __DIR__ . '/header-dashboard.php' ?>
 
-<div class="contenedir-sm">
+<div class="contenedor-sm">
     <div class="contenedor-nueva-tarea">
         <button
             type="button"
@@ -8,6 +8,53 @@
             id="agregar-tarea"
         >&#43; Nueva Tarea</button>
     </div>
+
+    <!--section  filtros[inicio] -->
+    <div
+        class="filtros"
+        id="filtros"
+    >
+        <div class="filtros-inputs">
+
+            <h2>Filtros</h2>
+            <!--section1 todas[inicio] -->
+            <div class="campo">
+                <label for="todas">Todas</label>
+                <input
+                    type="radio"
+                    id="todas"
+                    name="filtro"
+                    value=""
+                    checked
+                />
+            </div>
+            <!--!section1 fin - todas[fin] -->
+            <!--section2 completadas[inicio] -->
+            <div class="campo">
+                <label for="completadas">Completadas</label>
+                <input
+                    type="radio"
+                    id="completadas"
+                    name="filtro"
+                    value="1"
+                />
+            </div>
+            <!--!section2 fin - completadas[fin] -->
+            <!--section3 pendientes[inicio] -->
+            <div class="campo">
+                <label for="pendientes">Pendientes</label>
+                <input
+                    type="radio"
+                    id="pendientes"
+                    name="filtro"
+                    value="0"
+                />
+            </div>
+            <!--!section3 fin - pendientes[fin] -->
+        </div>
+    </div>
+    <!--!section  fin - filtros[fin] -->
+
     <!-- section espacio para mostrar las tareas[inicio]  -->
     <ul
         class="listado-tareas"
